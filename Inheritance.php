870622f815f039726e1a1f6dbe2d9ed2a4396243
= <alejosebastianabella@gmail.com>
2018-11-11 17:41:10 -0300

    inheritance

---
+++ Inheritance.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ *
+ */
+abstract class Shape
+{
+
+  protected $color;
+
+  public function __construct($color = 'red')
+  {
+    $this->color = $color;
+  }
+
+  public function getColor()
+  {
+    return $this->color;
+  }
+
+  abstract protected function getArea();
+}
+
+class Square extends Shape
+{
+  protected $length = 4;
+
+  public function getArea()
+  {
+    return pow($this->length, 2);
+  }
+
+}
+
+class Triangule extends Shape
+{
+  protected $base = 4;
+  protected $height = 7;
+
+  public function getArea()
+  {
+    return .5 * $this->base * $this->height;
+  }
+
+}
+
+echo (new Square('green'))->getColor();
